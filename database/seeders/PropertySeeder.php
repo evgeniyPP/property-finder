bd13bc3a29d03f58793e18ef1873e1690fb9219f
Evgeniy Petryankin <aysanru@gmail.com>
2021-02-23 12:51:09 +0300

    PropertySeeder added

---
+++ database/seeders/PropertySeeder.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Property;
+use Illuminate\Database\Seeder;
+
+class PropertySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Property::create([
+            'name' => 'The Victoria',
+            'price' => 374662,
+            'bedrooms' => 4,
+            'bathrooms' => 2,
+            'storeys' => 2,
+            'garages' => 2,
+        ]);
+        Property::create([
+            'name' => 'The Xavier',
+            'price' => 513268,
+            'bedrooms' => 4,
+            'bathrooms' => 2,
+            'storeys' => 1,
+            'garages' => 2,
+        ]);
+        Property::create([
+            'name' => 'The Como',
+            'price' => 454990,
+            'bedrooms' => 4,
+            'bathrooms' => 3,
+            'storeys' => 2,
+            'garages' => 3,
+        ]);
+        Property::create([
+            'name' => 'The Aspen',
+            'price' => 384356,
+            'bedrooms' => 4,
+            'bathrooms' => 2,
+            'storeys' => 2,
+            'garages' => 2,
+        ]);
+        Property::create([
+            'name' => 'The Lucretia',
+            'price' => 572002,
+            'bedrooms' => 4,
+            'bathrooms' => 3,
+            'storeys' => 2,
+            'garages' => 2,
+        ]);
+        Property::create([
+            'name' => 'The Toorak',
+            'price' => 521951,
+            'bedrooms' => 5,
+            'bathrooms' => 2,
+            'storeys' => 1,
+            'garages' => 2,
+        ]);
+        Property::create([
+            'name' => 'The Skyscape',
+            'price' => 263604,
+            'bedrooms' => 3,
+            'bathrooms' => 2,
+            'storeys' => 2,
+            'garages' => 2,
+        ]);
+        Property::create([
+            'name' => 'The Clifton',
+            'price' => 386103,
+            'bedrooms' => 3,
+            'bathrooms' => 2,
+            'storeys' => 1,
+            'garages' => 1,
+        ]);
+        Property::create([
+            'name' => 'The Geneva',
+            'price' => 390600,
+            'bedrooms' => 4,
+            'bathrooms' => 3,
+            'storeys' => 2,
+            'garages' => 2,
+        ]);
+    }
+}
