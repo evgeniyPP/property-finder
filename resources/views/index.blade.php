97a088b294e48942d6f8b86d68a19c541a983093
Evgeniy Petryankin <aysanru@gmail.com>
2021-02-24 14:11:21 +0300

    Force https scheme only in prod

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/app.css">
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <title>Property Finder</title>
 </head>
 <body>
